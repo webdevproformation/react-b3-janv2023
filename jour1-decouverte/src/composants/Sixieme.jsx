9ba04b130acf785112254e748961df008177f394
Malik <malik.h@webdevpro.net>
2023-01-16 15:05:21 +0100

    gestion des évènements

--- jour1-decouverte/src/composants/Sixieme.jsx
+++ jour1-decouverte/src/composants/Sixieme.jsx
@@ -1,12 +1,20 @@
 const Sixieme = () => {
     const action1 = ()=>{
-        alert("j'ai cliqué sur le bouton 1")
+        alert("j'ai cliqué sur le bouton 1");
+    }
+
+    const handleEnter = (e) => {
+        e.target.style["color"] = "red" ;
+    }
+    const handleLeave = (e) => {
+        e.target.style["color"] = "black";
     }
     return ( 
         <div>
             <h2>Evenement dans React</h2>
             <button onClick={action1}>action 1</button>
             <button onClick={() => action1()}>action 2</button>
+            <p onMouseEnter={handleEnter} onMouseLeave={handleLeave}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel placeat reiciendis facilis alias quam iste itaque, exercitationem, aspernatur eos reprehenderit nihil suscipit nam tempora doloremque eveniet autem. Quae, rem sint!</p>
         </div>
      );
 }
