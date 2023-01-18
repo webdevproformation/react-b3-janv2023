f6f0653e8d01c4bfbeb3a72ec0dcf98569022776
Malik <malik.h@webdevpro.net>
2023-01-18 16:49:52 +0100

    hook personnalisé

---
+++ jour3-blog/src/hook/useArticle.js
@@ -0,0 +1,21 @@
+import { useState , useEffect } from "react";
+import axios from "axios"
+
+export function useArticle(){
+    const [articles, setArticles] = useState([])
+    useEffect( () => {
+        axios.get(`${import.meta.env.VITE_API}articles.json`)
+         .then( reponse => {
+            const resultat = []
+            for(const key in reponse.data){
+                if(reponse.data[key]) resultat.push({...reponse.data[key] , id : key})
+            }
+            setArticles(resultat)
+            //console.log(resultat);
+            // { "0" : { "auteur" : "Alain" , "texte" : "super article" }, "1" : { "auteur" : "Alain" , "texte" : "beau boulot" } }
+            // [{ "auteur" : "Alain" , "texte" : "super article" } , ]
+         })
+    } , [] ) 
+
+    return articles ; 
+}
